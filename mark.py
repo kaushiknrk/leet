8724422e359ccbde3f9f62d0d1be403b60c6f24e
KAUSHIK NR <kaushiknr326@gmail.com>
2026-04-10 15:28:46 +0530

    average of three marks

---
+++ mark.py
@@ -0,0 +1,10 @@
+m1=int(input("Enter the mark1:"));
+m2=int(input("Enter the mark2:"));
+m3=int(input("Enter the mark3:"));
+if(0< m1,m2,m3 <101):
+   c=m1+m2+m3;
+   print(c);
+   print("Average mark of m1,m2,m3");
+   print((m1+m2+m3)/3);
+else:
+   print("please enter the mark correctly");
