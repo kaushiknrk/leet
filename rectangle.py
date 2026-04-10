7e75754977e16943a33a0f3e3cc0f64b2e81cd84
KAUSHIK NR <kaushiknr326@gmail.com>
2026-04-10 15:31:29 +0530

    Add rectangle area calculation and input validation

---
+++ rectangle.py
@@ -0,0 +1,6 @@
+l=int(input("Enter the length of the rectangle:"));
+b=int(input("Enter the breadth of the rectangle:"));
+if (0<= l,b <=100):
+   print("Area of rectangle:",l*b);
+else:
+   print("Invalid one:");
